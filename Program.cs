ae43d1754a76e042277a23bb09ab1774412dbf99
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-21 18:07:04 +0530

    Update Data From Table

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Address_Book_System_ADO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookAdv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookManagement addressBookManagement = new AddressBookManagement();
+
+
+            AddressBookModel model = new AddressBookModel();
+            model.FirstName = "Sangmeshwar";
+            model.LastName = "Patil";
+            model.Address = "Latur";
+            model.City = "Udgir";
+            model.State = "Maharashtra";
+            model.Zip = 413517;
+            model.PhoneNumber = 2635145678;
+            model.EmailId = "patil@gmail.com";
+            model.AddressBookType = "Contact";
+            model.AddressBookName = "AddressBook";
+            bool res = addressBookManagement.AddDataToTable(model);
+
+            Console.WriteLine(res);
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
