aae41ab7fda7b3e46fc1f582dda5153531d731f2
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-21 20:14:39 +0530

    Adding Contacts To Both Family And Friends

---
+++ AddressBookSQLQuery1.sql
@@ -0,0 +1,53 @@
+create database AddressBookServiceDB
+
+use AddressBookServiceDB
+create table AddressBook
+(
+firstName varchar(25),
+lastName varchar(25),
+address varchar(200),
+city varchar(25),
+state varchar(25),
+zip int,
+phoneNumber bigint,
+email varchar(100)
+);
+select * from AddressBook;
+
+
+insert into AddressBook values
+('Sangmeshwar','Patil','Latur','Udgir','MH',413517,8888888888,'patil@gmail.com'),
+('Pramod','Patil','Latur','Ausa','Maharashtra',413518,78577964412,'pramod@gmail.com')
+
+update AddressBook set zip =413517 where firstName='Pramod'
+delete from AddressBook where firstName='Pramod'
+
+insert into AddressBook values
+('Prajakta','Patil','Udgir','Latur','Maharashtra',413519,1111111111,'prajaktpatil@gmail.com')
+
+select * from AddressBook where city='Latur' or state='MH'
+
+select count(email) TotalPersons from AddressBook
+
+insert into AddressBook values
+('Basweshwar','Patil','NairCollege','Mumbai','Maharashtra',400003,4444444444,'basweshwar@gmail.com')
+
+select * from AddressBook where city ='Mumbai' order by firstName
+
+alter table AddressBook add type varchar(20),name varchar(20)
+update AddressBook set type='Superstars' where firstName='Sangmeshwar' or firstName='Prajakta'
+update AddressBook set name='MumbaiContacts' where firstName='Basweshwar' or firstName='Prajakta'
+update AddressBook set type='Me' where firstName='Sangmeshwar'
+update AddressBook set name='SelfContact' where firstName='Sangmeshwar'
+
+select count(email) from AddressBook where type='Superstars'
+
+insert into AddressBook values
+('Ritik','Pal','Nagpur','Nagpur','MH',729,76788,'ritik@gmail.com','Family','Brother'),
+('Pranay','Pal','Betul','MH','MP',729,76788,'pranay@gmail.com','Friend','Friend'),
+('Abbhi','G','KKatol','Nagp','MH',727,76688,'abhi@gmail.com','Family','BigBrother'),
+('pragya','G','Kolkata123','Nagpur','MH',727,76688,'pragya@gmail.com','Friend','Sister'),
+('Pragati','Banerjee','Amravati','Amravati','MH',123,77777,'pragati@gmail.com','Family','Mom'),
+('Tushar','Banerjee','Nagpur','Nagpur','MH',123,78888,'tushar@gmail.com','Family','Father'),
+('Rounak','Ghosh','Kolkata123','Kolkata','West Bengal',345,77888,'ranak@gmail.com','Friend','Bro'),
+('Sameer','Ali','Delhi123','Delhi','Haryana',789,77788,'sameer@gmail.com','Friend','Uncle')
