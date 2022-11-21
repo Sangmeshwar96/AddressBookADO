2729548df3c34f546feff806861d37e0e4837fce
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-21 19:17:09 +0530

    Getting Size Of Address Book

---
+++ AddressBookSQLQuery1.sql
@@ -0,0 +1,30 @@
+create database AddressBookServiceDB
+
+use AddressBookServiceDB
+create table AddressBook
+(
+firstName varchar(25),
+lastName varchar(25),
+address varchar(200),
+city varchar(25),
+state varchar(25),
+zip int,
+phoneNumber bigint,
+email varchar(100)
+);
+select * from AddressBook;
+
+
+insert into AddressBook values
+('Sangmeshwar','Patil','Latur','Udgir','MH',413517,8888888888,'patil@gmail.com'),
+('Pramod','Patil','Latur','Ausa','Maharashtra',413518,78577964412,'pramod@gmail.com')
+
+update AddressBook set zip =413517 where firstName='Pramod'
+delete from AddressBook where firstName='Pramod'
+
+insert into AddressBook values
+('Prajakta','Patil','Udgir','Latur','Maharashtra',413519,1111111111,'prajaktpatil@gmail.com')
+
+select * from AddressBook where city='Latur' or state='MH'
+
+select count(email) TotalPersons from AddressBook
