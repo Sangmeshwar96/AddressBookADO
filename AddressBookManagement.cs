a629f9bacfa4440d474e75b5aeb8c09d22218846
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-21 17:46:53 +0530

    Address Book System Using ADO

---
+++ AddressBookManagement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System_ADO
+{
+    class AddressBookManagement
+    {
+        
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBookServiceDB;Integrated Security=true;";
+         
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public void DataBaseConnection()
+        {
+            try
+            {
+ 
+                DateTime now = DateTime.Now;
+
+                connection.Open();
+
+                using (connection)
+                {
+
+                    Console.WriteLine("Connection is created Successful. ");
+                }
+ 
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
